3b27549e8ac2f683d7f1b6b2ba890a83b3bd2b97
Egor <sychyov1991@mail.ru>
2022-04-24 19:36:47 +0600

    Create new doc

---
+++ BusinessLayer/Documents/Services/CreateDoc.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Documents.Models;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace BusinessLayer.Documents.Services
+{
+    public static class CreateDoc
+    {
+        private static string filepath = @"c:\Users\Public\Documents\";
+        public static void CreateProxy(AddNewDocRequest request)
+        {
+            try
+            {
+                var filename = $"{filepath}Proxy-{Guid.NewGuid()}.docx";
+                using (WordprocessingDocument wordDocument =
+                WordprocessingDocument.Create(filename, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                    mainPart.Document = new Document();
+                    Body body = mainPart.Document.AppendChild(new Body());
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+                    run.AppendChild(new Text(request.ActionDescription));
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
--- BusinessLayer/Documents/Services/DocumentService.cs
+++ BusinessLayer/Documents/Services/DocumentService.cs
@@ -29,6 +29,8 @@ namespace BusinessLayer.Documents.Services
                 Type = request.Type,
                 СonfidantId = request.СonfidantId
             });
+
+            CreateDoc.CreateProxy(request);
         }
     }
 }
